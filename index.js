c59413b7dfb76d18fb5a65e63320dc60f61dd758
Erick <santoserick9@gmail.com>
2021-08-05 00:14:56 -0300

    create functions that adding and remove wrapper player class

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("start");
+const playerWrapperEl = document.querySelector(".wrapper_player");
+const playerBottomEl = document.querySelector(".player_bottom");
+const playerCloseEl = document.querySelector(".close");
+
+const activeClass = "active";
+
+const addActiveClassToPlayerWrapper = (e) => {
+  const tagName = e.target.tagName;
+
+  if (tagName !== "BUTTON") {
+    playerWrapperEl.classList.add(activeClass);
+  }
+};
+
+playerBottomEl.addEventListener("click", addActiveClassToPlayerWrapper);
+
+playerCloseEl.addEventListener("click", () =>
+  playerWrapperEl.classList.remove(activeClass)
+);
