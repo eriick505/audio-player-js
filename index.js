db2e61ddcb63628a097280264f4f406a81d038a9
Erick <santoserick9@gmail.com>
2021-08-05 18:21:14 -0300

    gitignore

--- index.js
+++ index.js
@@ -17,3 +17,11 @@ playerBottomEl.addEventListener("click", addActiveClassToPlayerWrapper);
 playerCloseEl.addEventListener("click", () =>
   playerWrapperEl.classList.remove(activeClass)
 );
+
+const API_BASE = "http://localhost:8080";
+const ENDPOINT_FILE_LIST = "api/v1/list-files";
+
+console.log(`${API_BASE}/${ENDPOINT_FILE_LIST}`);
+fetch(`${API_BASE}/${ENDPOINT_FILE_LIST}`)
+  .then((r) => r.json())
+  .then((json) => console.log(json));
